09e71923ffb583ac73bb126fdb74e771f7a6c0e0
MohammedAB <albay.m@hotmail.com>
2024-01-10 15:25:38 +0100

    Copy solutions from exercice 6

---
+++ Exercice 7/Beverage.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+class Beverage
+{
+  // Properties
+  private string $color;
+  private float $price;
+  private string $temperature;
+
+  // Construct
+  public function __construct(string $color, float $price)
+  {
+    $this->color = $color;
+    $this->price = $price;
+    $this->temperature = "cold";
+  }
+
+  // The function
+  public function getInfo() 
+  {
+    return "This beverage is $this->temperature, has a $this->color color and costs &euro;$this->price.";
+  }
+
+  // Make function to get the global constant
+  public function get_globalConstant()
+  {
+    return BARNAME;
+  }
+}
