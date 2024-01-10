ac009bc045a0ad1e6439275792f992423ddd738d
MohammedAB <albay.m@hotmail.com>
2024-01-10 15:35:47 +0100

    Create protected static property for

--- Exercice 7/Beverage.php
+++ Exercice 7/Beverage.php
@@ -8,6 +8,8 @@ class Beverage
   private string $color;
   private float $price;
   private string $temperature;
+  // static property that can only be accessed from inside this class (= protected) called 'address' which has the value "Melkmarkt 9, 2000 Antwerpen"
+  protected static string $address = "Melkmarkt 9, 2000 Antwerpen";
 
   // Construct
   public function __construct(string $color, float $price)
