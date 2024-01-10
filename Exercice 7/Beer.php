d13bb54667218e44511e0e3ade1b8a9b69431c5d
MohammedAB <albay.m@hotmail.com>
2024-01-10 15:24:57 +0100

    Copy solutions from exercice 6

---
+++ Exercice 7/Beer.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+include_once "Beverage.php";
+
+// Beer is inherited from Beverage through the keyword 'extends'
+class Beer extends Beverage
+{
+  // Properties
+  private string $name;
+  private float $alcoholPercentage;
+
+  // Construct function
+  public function __construct(string $color, float $price, string $name, float $alcoholPercentage) 
+  {
+    $this->color = $color;
+    $this->price = $price;
+    $this->name = $name;
+    $this->alcoholPercentage = $alcoholPercentage;
+  }
+
+  // Make a get_AlcoholPercentage function to get the alcohol percentage
+  public function get_AlcoholPercentage()
+  {
+    return $this->alcoholPercentage;
+  }
+
+  // Make a function to get the global constant
+  public function get_globalConstantFromBeer()
+  {
+    return BARNAME;
+  }
+}
